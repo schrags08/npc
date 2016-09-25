775a6c01f4f5e0a9908eafa101a62b9a354892ef
Matt Schragal <matt.schragal@morningstar.com>
2016-09-25 01:42:16 -0500

    WIP - add nav - add history page - add cheesy surprise

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,4 +1,7 @@
+const CSS_PREFIX = 'npc';
+
 window.onload = function () {
+    initAskCheesyAnimation();
     initEasterEgg();
 };
 
@@ -7,8 +10,14 @@ function initEasterEgg() {
 
     new Konami(function () {
         var status = !isEggEnabled ? 'e]\[@bl3D' : 'diZ481Ed';
-        $('body').toggleClass('npc--l33tMode');
+        $('body').toggleClass(`${CSS_PREFIX}--l33tMode`);
         console.log(`H4x0r l337 ]\/[0D3 ${status}...`);
         isEggEnabled = !isEggEnabled;
     });
+}
+
+function initAskCheesyAnimation() {
+    $(`li.${CSS_PREFIX}__askCheesy`).hover(function() {
+        $(this).toggleClass(`${CSS_PREFIX}__askCheesy--hover`);
+    })
 }
