185f32d2a19c0d14334753553456c28a56ffdae3
Matt Schragal <matt.schragal@morningstar.com>
2017-06-05 01:49:48 -0500

    rough cut for first deploy - sign up for updates working - reg working

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,11 +1,13 @@
 const CSS_PREFIX = 'npc';
 
 $(function() {
-    initAskCheesyAnimation();
+    // initAskCheesyAnimation();
     initFlickity();
     initMacy();
     initCountdown();
-    initEasterEgg();
+    // initEasterEgg();
+    initSignUp();
+    initRegistration();
 });
 
 function initAskCheesyAnimation() {
@@ -61,5 +63,103 @@ function initCountdown() {
                     document.getElementById('countdown').innerHTML = ts.toHTML();
                 },
                 countdown.DAYS | countdown.HOURS | countdown.MINUTES | countdown.SECONDS);
+    }
+}
+
+function initSignUp() {
+    var button = document.querySelector('button[name="signup"]');
+
+    if (button) {
+        button.addEventListener('click', e => {
+            button.disabled = true;
+            button.innerHTML = 'Processing';
+
+            var name = document.querySelector('input[name="name"]');
+            var email = document.querySelector('input[name="email"]');
+            var updatesForm = document.querySelector(`.${CSS_PREFIX}__updates-form`);
+            var updatesMessage = document.querySelector(`.${CSS_PREFIX}__updates-message`);
+            var url = `https://neighborhoodpubcrawl.azurewebsites.net/api/AddPatronTest?name=${encodeURIComponent(name.value)}&email=${encodeURIComponent(email.value)}`;
+            
+            updatesForm.style.display = 'block';
+            updatesMessage.classList.remove('success', 'alert');
+            updatesMessage.innerHTML = '';
+            updatesMessage.style.display = 'none';
+
+            $.get(url)
+                .done(data => {
+                    updatesMessage.classList.add('success');
+                    updatesMessage.innerHTML = `<p>Welcome aboard!</p>`;
+                })
+                .fail(data => {
+                    updatesMessage.classList.add('alert');
+                    updatesMessage.innerHTML = `<p>Oops, something went wrong! Try again later.</p>`;
+                }).always(() => {
+                    updatesMessage.style.display = 'block';
+                    updatesForm.style.display = 'none';
+                });
+        });
+    }
+}
+
+function initRegistration() {
+    var payment = document.querySelector('select[name="payment"]');
+    var size = document.querySelector('select[name="size"]');
+
+    if (payment) {
+        payment.addEventListener('change', paymentChange);
+    }
+
+    if (size) {
+        size.addEventListener('change', sizeChange);
+    }
+
+    function paymentChange() {
+        let index = payment.selectedIndex;
+        
+        reset();
+
+        switch(index) {
+            case 1:
+                showVenmo();
+                break;
+            case 2:
+                showPayPal();
+                break;
+            default:
+                reset();
+                break;
+        }
+    }
+
+    function sizeChange() {
+        var sizeIndex = size.selectedIndex;
+        var sizeOption = size.options[sizeIndex].value;
+        
+        var url = `https://venmo.com/?txn=pay&audience=private&recipients=schrags08&amount=30&note=NPC2017-${sizeOption}`;
+
+        if (sizeIndex > 0) {
+            $('.checkout--venmo').show();
+            $('.checkout--venmo').find('a').attr('href', url);
+        } else {
+            $('.checkout--venmo').hide();
+        }
+    }
+
+    function reset() {
+        size.selectedIndex = 0;
+
+        $('.modifier').hide();
+        $('.checkout').hide();
+    }
+
+    function showVenmo() {
+        $('.modifier').show();
+        $('.checkout').hide();
+    }
+
+    function showPayPal() {
+        $('.modifier').hide();
+        $('.checkout').hide();
+        $('.checkout--paypal').show();
     }
 }
