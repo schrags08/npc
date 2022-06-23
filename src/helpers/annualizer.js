ebf5bf999b0d6524c6e667f2858109ff64a6dd8f
schrags08 <schrags08@hotmail.com>
2022-06-23 17:28:47 -0500

    Update dependencies and revise content for 2022

--- src/helpers/annualizer.js
+++ src/helpers/annualizer.js
@@ -1,10 +1,12 @@
 var moment = require('moment');
 var ordinalizer = require('./ordinalizer');
 
+const YEARS_LOST_TO_COVID = 2;
+
 module.exports = function(first, current) {
-    var dtFirst = new Date(first),
-        dtCurrent = new Date(current),
-        years = Number(moment(dtCurrent).diff(dtFirst, 'years'));
+    var dtFirst = new Date(first);
+    var dtCurrent = new Date(current);
+    var years = Number(moment(dtCurrent).diff(dtFirst, 'years')) - YEARS_LOST_TO_COVID;
 
     return ordinalizer(years + 1);
 };
